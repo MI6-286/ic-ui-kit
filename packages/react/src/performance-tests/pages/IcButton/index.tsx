15213ac2480338b8301ea17d00c08a26a852fb3a
jd239 <116016939+jd239@users.noreply.github.com>
2024-09-14 20:05:18 +0000

    feat(react): created workflow for performance testing
    Added workflow for performance testing which includes:
    - create a simple react app
    - setting up webpack config to develop and build react app
    - installed cypress lighthouse tooling
    - run lighthouse against react app
    - node script to kill port once tests are complete

---
+++ packages/react/src/performance-tests/pages/IcButton/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IcButton } from "../../../components";
+
+const IcButtonPage = () => {
+  return (
+    <>
+      <h1>Button Page</h1>
+      <IcButton>Test Button</IcButton>
+    </>
+  );
+};
+
+export default IcButtonPage;
