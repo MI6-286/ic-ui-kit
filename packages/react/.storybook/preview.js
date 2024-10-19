f348b96a2c1f66044c7ded49cf44e409efbe1818
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-10-19 20:29:18 +0100

    perf(react): add the ability to change between themes in Storybook toolbar

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -4,15 +4,56 @@ import "../dist/core/core.css";
 import "./storybook-overrides.css";
 
 import { withPerformance } from 'storybook-addon-performance';
+import { IcTheme } from "../src/components";
 
-export const parameters = {
-    controls: { 
+const preview = {
+  parameters: {
+    controls: {
         matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
       hideNoControlsWarning: true
-    }
-  }
+    },
+  },
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      toolbar: {
+        title: 'Theme',
+        icon: 'paintbrush',
+        items: [{value: 'light', title: 'Light theme', left: '☀️'}, {value:'dark', title: 'Dark theme', left: '🌙'}],
+        dynamicTitle: true,
+      },
+    },
+  },
+  initialGlobals: {
+    theme: 'light',
+  },
+  decorators: [
+    (story, context) => {
+      const selectedTheme = context.globals.theme;
+      const backgroundColor = selectedTheme === 'dark' ? 'var(--ic-color-background-primary-dark)' : 'var(--ic-color-background-primary-light)';
+      return (
+        <>
+          <style>
+            {`
+              .sb-show-main {
+                background-color: ${backgroundColor};
+              }
+            `}
+          </style>
+          <IcTheme 
+            theme={selectedTheme}
+            style={{ backgroundColor: backgroundColor }}
+          >
+            {story()}
+          </IcTheme>
+        </>
+      );
+    },
+    withPerformance
+  ],
+};
 
-export const decorators = [withPerformance];
+export default preview;
